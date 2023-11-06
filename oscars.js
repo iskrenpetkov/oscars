86e65882512a9ef815408edd254200ee39475400
iskrenpetkov <145710509+iskrenpetkov@users.noreply.github.com>
2023-11-06 15:05:03 +0200

    Add files via upload

---
+++ oscars.js
@@ -0,0 +1,44 @@
+function oscars(input) {
+
+    let index = 0;
+    let name = input[index];
+    index++;
+    let points = Number(input[index])
+    index++;
+    let n = Number(input[index])
+    index++;
+
+    for (let i = 0; i < n; i++) {
+        let jName=input[index];
+        index++;
+        let currentPoints = Number(input[index]);
+        index++;
+        currentPoints=jName.length*currentPoints/2;
+        points+=currentPoints;
+        if (points>1250.5) {
+            break;
+        }
+    }
+    if (points>1250.5) {
+        console.log(`Congratulations, ${name} got a nominee for leading role with ${points.toFixed(1)}!`);
+    }else{
+        let diff=Math.abs(points-1250.5)
+        console.log(`Sorry, ${name} you need ${diff.toFixed(1)} more!`);
+    }
+
+
+
+
+
+}
+oscars(["Zahari Baharov",
+    "205",
+    "4",
+    "Johnny Depp",
+    "45",
+    "Will Smith",
+    "29",
+    "Jet Lee",
+    "10",
+    "Matthew Mcconaughey",
+    "39"])
